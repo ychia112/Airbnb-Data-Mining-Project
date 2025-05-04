3e7ab3c63f42305a509303420d4106094e569820
ychia <jackyjacky63@gmail.com>
2025-05-03 20:21:08 -0700

    0503 02_Statistical_Analysis done

--- src/stats_tests.py
+++ src/stats_tests.py
@@ -6,12 +6,13 @@ import os
 
 def ttest(df, group1, group2):
     t_stat, p_val = stats.ttest_ind(group1, group2, equal_var=False)
-    return t_stat, p_val
+    return print(f"T-test:\nT-statistic: {t_stat:.2f}\nP-value: {p_val}")
 
 def anova(df, group: str):
     groups = [df[df[group] == level]["nightly_rate"].values for level in df[group].unique()]
     f_stat, p_val = stats.f_oneway(*groups)
-    return (f"ANOVA: {group}", f_stat, p_val)
+    
+    return print(f"ANOVA: {group}\nF-statistic: {f_stat:.2f}\nP-value: {p_val}")
 
 def plot_group_distributions(df, column):
     plt.figure(figsize=(8, 6))
