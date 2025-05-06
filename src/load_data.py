5a83c93f29ab1ab37f572d71f83a9987de7c2731
ychia <jackyjacky63@gmail.com>
2025-05-06 12:26:07 -0700

    0506 01_EDA clearify variables

--- src/load_data.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-def load_data(relative_path):
-    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    full_path = os.path.join(project_root, relative_path)
-    
-    return pd.read_csv(full_path)
